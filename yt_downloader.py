bd952ddc4ccf3da390b0da99499d6a497e72f9ab
zosbourne9 <zosbourne9@gmail.com>
2023-10-19 19:41:20 -0400

    Create yt_downloader.py

---
+++ yt_downloader.py
@@ -0,0 +1,88 @@
+import customtkinter as ctk
+import tkinter
+from pytube import YouTube
+from moviepy.editor import *
+import os
+
+def dl_video():
+    url = entry_url.get()
+    f_type = filetype_var.get()
+    
+    status_label.pack(pady=(10,5))
+    progress_label.pack(pady=(10,5))
+    progress_bar.pack(pady=(10,5))
+    
+    try:
+        yt = YouTube(url, on_progress_callback=on_progress)
+        
+        if f_type == "MP4":
+            stream = yt.streams.get_highest_resolution().download(output_path=f"{os.environ['UserProfile']}/Desktop")
+            status_label.configure(text=f"Sucessfully Downloaded {yt.title}", text_color = "white", fg_color="green")
+        elif f_type == "MP3":
+            stream = yt.streams.get_audio_only().download(output_path=f"{os.environ['UserProfile']}/Desktop")
+            status_label.configure(text=f"Sucessfully Downloaded {yt.title}", text_color = "white", fg_color="green")
+            clip = AudioFileClip(stream)
+            clip.write_audiofile(stream[:-4] + ".mp3")
+            clip.close()
+            os.remove(stream)
+                
+    except Exception as e:
+        status_label.configure(text=f"Error: {str(e)}", text_color = "white", fg_color="red")
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_completed = bytes_downloaded / total_size * 100
+    
+    progress_label.configure(text= str(int(percentage_completed)) + "%")
+    progress_label.update()
+    
+    progress_bar.set(float(percentage_completed / 100))
+
+# System settings
+app = ctk.CTk()
+ctk.set_appearance_mode("system")
+ctk.set_default_color_theme("blue")
+
+# App title
+app.title("Youtube Downloader")
+
+# set min and max width and height
+app.geometry("720x480")
+app.minsize(720, 480)
+app.maxsize(1080, 720)
+
+# create frame to hold content
+app_frame = ctk.CTkFrame(app)
+app_frame.pack(fill=ctk.BOTH, expand=True, padx=10, pady=10)
+
+# create a label and the entry widget for the url
+url_label = ctk.CTkLabel(app_frame, text="Enter the youtube url here: ")
+entry_url = ctk.CTkEntry(app_frame, width=400, height=40)
+url_label.pack(pady=(10,5))
+entry_url.pack(pady=(10,5))
+
+# create a file type combo box
+filetype = ["MP3", "MP4"]
+filetype_var = ctk.StringVar()
+filetype_label = ctk.CTkLabel(app_frame, text="Choose File Type")
+filetype_label.pack(pady=(10,5))
+filetype_combobox = ttk.Combobox(app_frame, values=filetype, textvariable=filetype_var)
+filetype_combobox.pack(pady=(10,5))
+filetype_combobox.set("File Type")
+
+# create a download button
+dl_button = ctk.CTkButton(app_frame, text="Download", command=dl_video)
+dl_button.pack(pady=(10,5))
+
+# create a label and progress bar to display download progress
+progress_label = ctk.CTkLabel(app_frame, text="0%")
+
+progress_bar = ctk.CTkProgressBar(app_frame, width=400)
+progress_bar.set(0)
+
+# create status level
+status_label = ctk.CTkLabel(app_frame, text="")
+
+# Run app
+app.mainloop()
